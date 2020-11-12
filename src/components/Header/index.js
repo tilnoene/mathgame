38517e86aea3d770129c25535e2bfb56aae06668
Victor Manuel <victormanuel@cjr.org.br>
2020-11-12 18:33:47 -0300

    Modificando Header e estilizaçao de Buttons no mobile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import "./styles.css";
 
 const Header = () => (
-    <div id="anuncio-header">Anúncio</div> 
+    <div id="anuncio-header"></div> 
 );
 
 export default Header;
